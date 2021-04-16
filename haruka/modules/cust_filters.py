66cd1e207e216207c7d5810eed25b6ccedce724d
ez69bw <81008242+ez69bw@users.noreply.github.com>
2021-04-16 07:08:47 +0700

    Update cust_filters.py

--- haruka/modules/cust_filters.py
+++ haruka/modules/cust_filters.py
@@ -324,7 +324,9 @@ __mod_name__ = "Filters"
 FILTER_HANDLER = DisableAbleCommandHandler("filter", filters)
 STOP_HANDLER = DisableAbleCommandHandler("stop", stop_filter)
 LIST_HANDLER = DisableAbleCommandHandler("filters", list_handlers, admin_ok=True)
-CUST_FILTER_HANDLER = MessageHandler(CustomFilters.has_text, reply_filter)
+CUST_FILTER_HANDLER = MessageHandler(
+    CustomFilters.has_text & ~Filters.update.edited_message, reply_filter, run_async=True
+)
 
 dispatcher.add_handler(FILTER_HANDLER)
 dispatcher.add_handler(STOP_HANDLER)
