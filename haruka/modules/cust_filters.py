de39bcc5969632da3879997779e2808ad215eb0a
ez69bw <81008242+ez69bw@users.noreply.github.com>
2021-04-18 20:52:44 +0700

    Update cust_filters.py

--- haruka/modules/cust_filters.py
+++ haruka/modules/cust_filters.py
@@ -194,15 +194,15 @@ def reply_filter(bot: Bot, update: Update):
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
         if re.search(pattern, to_match, flags=re.IGNORECASE):
             filt = sql.get_filter(chat.id, keyword)
-                if filt.is_text:
-                    if "%%%" in filt.is_text:
-                        split = filt.is_text.split("%%%")
+                if filt.reply_text:
+                    if "%%%" in filt.reply_text:
+                        split = filt.reply_text.split("%%%")
                         if all(split):
                             text = random.choice(split)
                         else:
-                            text = filt.is_text
+                            text = filt.reply_text
                     else:
-                        text = filt.is_text
+                        text = filt.reply_text
                     if text.startswith("~!") and text.endswith("!~"):
                         sticker_id = text.replace("~!", "").replace("!~", "")
                         try:
