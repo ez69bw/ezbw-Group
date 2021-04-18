3bf7b6be4f928e22ab686c705a42c0442da707dc
ez69bw <81008242+ez69bw@users.noreply.github.com>
2021-04-18 20:12:56 +0700

    Update cust_filters_sql.py

--- haruka/modules/sql/cust_filters_sql.py
+++ haruka/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from AstrakoBot.modules.helper_funcs.msg_types import Types
-from AstrakoBot.modules.sql import BASE, SESSION
+from haruka.modules.helper_funcs.msg_types import Types
+from haruka.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
