63dcf1ec5c43316c22f2b09a44027404a6e7a324
ez69bw <81008242+ez69bw@users.noreply.github.com>
2021-04-28 15:41:58 +0700

    Add files via upload

--- haruka/modules/helper_funcs/filters.py
+++ haruka/modules/helper_funcs/filters.py
@@ -1,21 +1,27 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from haruka import SUPPORT_USERS, SUDO_USERS
+from Mizuki import DEMONS, DEV_USERS, DRAGONS
 
 
 class CustomFilters(object):
     class _Supporters(BaseFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
+            return bool(message.from_user and message.from_user.id in DEMONS)
 
     support_filter = _Supporters()
 
     class _Sudoers(BaseFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUDO_USERS)
+            return bool(message.from_user and message.from_user.id in DRAGONS)
 
     sudo_filter = _Sudoers()
+
+    class _Developers(BaseFilter):
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in DEV_USERS)
+
+    dev_filter = _Developers()
 
     class _MimeType(BaseFilter):
         def __init__(self, mimetype):
@@ -23,12 +29,20 @@ class CustomFilters(object):
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
 
         def filter(self, message: Message):
-            return bool(message.document and message.document.mime_type == self.mime_type)
+            return bool(
+                message.document and message.document.mime_type == self.mime_type
+            )
 
     mime_type = _MimeType
 
     class _HasText(BaseFilter):
         def filter(self, message: Message):
-            return bool(message.text or message.sticker or message.photo or message.document or message.video)
+            return bool(
+                message.text
+                or message.sticker
+                or message.photo
+                or message.document
+                or message.video
+            )
 
     has_text = _HasText()
